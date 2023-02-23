1b774e92d3955887e5f7494e31e79c831b21fa7e
beggsfinest00 <81796822+beggsfinest00@users.noreply.github.com>
2023-02-23 12:47:35 -0600

    Initial commit

---
+++ HelloWorld_Holbrook_Dallas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld_Holbrook_Dallas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Ask user for First Name
+            Console.WriteLine("What is your First Name?");
+            string firstName = Console.ReadLine();
+
+            // Ask user for Last Name
+            Console.WriteLine("What is your Last Name?");
+            String lastName = Console.ReadLine();
+
+            // Makes Joke hehe.HaHa
+            Console.WriteLine("What is your SSN for !!security reasons only!!");
+            String SSN = Console.ReadLine();
+
+            // Responds to user
+            Console.WriteLine($"Hello, {firstName} {lastName} {SSN}!");
+        }
+    }
+}
